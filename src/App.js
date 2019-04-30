721b631303ec4ef7d06f8e6150398f05541e3f0d
Dustin Joynt <dustinjoynt@gmail.com>
2019-04-30 14:33:21 -0600

    added movie detail component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import NavBar from "./components/NavBar"
-import MainCarousel222 from "./components/Landing/MainCarousel222"
+// import MainCarousel222 from "./components/Landing/MainCarousel222"
+import MovieDetail from './components/MovieDetail'
 
 import { requestMovies } from './actions'
 
@@ -24,11 +25,29 @@ class App extends Component {
   componentDidMount() {
     this.props.onRequestMovies()
   }
+
   render(){
-  return (
+    const { movies, isPending, error } = this.props
+    console.log('aap', movies)
+    return isPending ? 
+    <h1>loading....</h1> :
+
+
+  
+    // const movieDetail = movies.map( movie => movie.results)
+    // console.log(movieDetail[0])
+    // for(let x in movies[0]){
+    //   console.log(x)
+    // }
+
+    // setTimeout(() => {
+    //   console.log(movies[0].results[0].title)
+    // }, 3000)
+    
+  (
     <div className = "App">
-      <NavBar/>
-      <MainCarousel222/>
+      <NavBar />
+      <MovieDetail movies = {movies} />
     </div>
   )
   }
