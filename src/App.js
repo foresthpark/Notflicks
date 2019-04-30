aa604a6dcbcac07cf59a102a8f17d531ae22ba8a
Forest Park <foresthpark@gmail.com>
2019-04-30 14:56:43 -0600

    resolve barnch conflict

--- src/App.js
+++ src/App.js
@@ -34,30 +34,29 @@ class App extends Component {
     this.props.onRequestMovies()
   }
 
-  render(){
-  //   const { movies, isPending, error } = this.props
-  //   console.log('aap', movies)
-  //   return isPending ? 
-  //   <h1>loading....</h1> :
+  render() {
+    //   const { movies, isPending, error } = this.props
+    //   console.log('aap', movies)
+    //   return isPending ?
+    //   <h1>loading....</h1> :
 
 
-  
-  //   // const movieDetail = movies.map( movie => movie.results)
-  //   // console.log(movieDetail[0])
-  //   // for(let x in movies[0]){
-  //   //   console.log(x)
-  //   // }
+    //   // const movieDetail = movies.map( movie => movie.results)
+    //   // console.log(movieDetail[0])
+    //   // for(let x in movies[0]){
+    //   //   console.log(x)
+    //   // }
 
-  //   // setTimeout(() => {
-  //   //   console.log(movies[0].results[0].title)
-  //   // }, 3000)
-    
-  // (
-  //   <div className = "App">
-  //     <NavBar />
-  //     <MovieDetail movies = {movies} />
-  //   </div>
-  // )
+    //   // setTimeout(() => {
+    //   //   console.log(movies[0].results[0].title)
+    //   // }, 3000)
+
+    // (
+    //   <div className = "App">
+    //     <NavBar />
+    //     <MovieDetail movies = {movies} />
+    //   </div>
+    // )
 
     const images = ["https://via.placeholder.com/600/92c952",
       "https://via.placeholder.com/600/771796",
@@ -85,6 +84,7 @@ class App extends Component {
       </div>
     )
 
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App) 
+export default connect(mapStateToProps, mapDispatchToProps)(App)
