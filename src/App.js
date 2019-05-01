888caffbc361707a59127fc0d0712ae7c5159cca
Dustin Joynt <dustinjoynt@gmail.com>
2019-05-01 11:15:44 -0600

    added redux action for movie details

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const mapStateToProps = (state) => {
     isPending: state.requestMovies.isPending,
     movies: state.requestMovies.movies,
     error: state.requestMovies.error,
-    MovieDetail: state.getMovieDetail.MovieDetail,
+    movieId: state.getMovieDetail.movieId,
     renderDetail: state.getMovieDetail.renderDetail
   }
 }
@@ -36,8 +36,7 @@ class App extends Component {
 
 
   render() {
-    console.log('render', this.props.renderDetail)
-    const {movies, isPending, onGetMovieDetail, renderDetail} = this.props
+    const {movies, isPending, onGetMovieDetail, renderDetail, movieId} = this.props
 
     return isPending ? <Loading/> :
       (
@@ -56,7 +55,9 @@ class App extends Component {
           }
 
           {renderDetail === true &&
-          <MovieDetail/>
+          <MovieDetail
+            movie = {movieId}
+          />
           }
         </div>
       )
--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class MovieDetail extends Component{
   render(){
+    console.log('movieDetail', this.props.movie)
     return(
       <div> 
       <h1>Hello World</h1>
