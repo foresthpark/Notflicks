1c95f9a3c2831d50e9edfd4154549355d0ed244a
Forest Park <foresthpark@gmail.com>
2019-05-02 10:11:19 -0600

    fixed movie player

--- src/components/cards/MovieTrailer.jsx
+++ src/components/cards/MovieTrailer.jsx
@@ -5,22 +5,19 @@ class MovieTrailer extends React.Component {
   render() {
     const {movie} = this.props
     const videoURL = "https://www.youtube.com/watch?v="
+    // let videoKey = movie[3].results[0].key
 
-    let videoKey;
-    if (movie[3].results.length > 0) {
-      let videoKey = movie[3].results[0].key
-    } else {
-      let videoKey = null
-    }
+    let videoKey = movie[3].results[0] ? movie[3].results[0].key : null
 
     const videoSuffix = "?controls=0"
     const fullURL = `${videoURL}${videoKey}${videoSuffix}`
 
+
     return (
       <div>
-        {movie[3].results.length > 0 &&
-        <ReactPlayer url={fullURL}/>
-        }
+
+        {videoKey ? <ReactPlayer url={fullURL}/> : <div>Sorry. This don't work</div>}
+
       </div>
     );
   }
