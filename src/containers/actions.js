38c810183e315b117da5e1cb6f01a9d7434f4307
Dustin Joynt <dustinjoynt@gmail.com>
2019-05-06 09:35:40 -0600

    fixed comments

--- src/containers/actions.js
+++ src/containers/actions.js
@@ -12,9 +12,9 @@ import {
 
 const urls = [
   'https://api.themoviedb.org/3/movie/top_rated?api_key=5abdca519fcfe51f18cec09aefcec6b4&language=en-US&page=1',
-  // 'https://api.themoviedb.org/3/movie/now_playing?api_key=5abdca519fcfe51f18cec09aefcec6b4&language=en-US&page=1',
-  // 'https://api.themoviedb.org/3/movie/upcoming?api_key=5abdca519fcfe51f18cec09aefcec6b4&language=en-US&page=1',
-  // 'https://api.themoviedb.org/3/movie/popular?api_key=5abdca519fcfe51f18cec09aefcec6b4&language=en-US&page=1'
+  'https://api.themoviedb.org/3/movie/now_playing?api_key=5abdca519fcfe51f18cec09aefcec6b4&language=en-US&page=1',
+  'https://api.themoviedb.org/3/movie/upcoming?api_key=5abdca519fcfe51f18cec09aefcec6b4&language=en-US&page=1',
+  'https://api.themoviedb.org/3/movie/popular?api_key=5abdca519fcfe51f18cec09aefcec6b4&language=en-US&page=1'
 ]
 
 export const requestMovies = () => async (dispatch) => {
